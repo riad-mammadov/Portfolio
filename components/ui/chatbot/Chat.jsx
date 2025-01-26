944da2f3eec1e2ec53288feb7aad2e3c98f57ff3
Riad <riad_mammadov@yahoo.com>
2025-01-26 21:39:56 +0000

    improve design and functionality

--- components/ui/chatbot/Chat.jsx
+++ components/ui/chatbot/Chat.jsx
@@ -2,18 +2,30 @@
 import { useEffect, useRef, useState } from "react";
 import ChatForm from "./chatform";
 import ChatMessage from "./ChatMessage";
-import { Bot } from "lucide-react";
+import { Bot, Dot } from "lucide-react";
 import { aiPrompt } from "@/utils/ai";
+import { motion } from "framer-motion";
 
 function Chatbox() {
-  const [chatHistory, setChatHistory] = useState([]);
+  const [chatHistory, setChatHistory] = useState([
+    {
+      role: "model",
+      text: "Hello, I'm Riad!",
+    },
+    {
+      role: "model",
+      text: "Feel free to ask anything about me, my education, projects and hobbies, i'm happy to share!",
+    },
+  ]);
   const ref = useRef(null);
+  const [loading, setIsLoading] = useState(false);
 
   function updateHistory(text, isError = false) {
     setChatHistory((prev) => [...prev, { role: "model", text, isError }]);
   }
 
   async function generateBotResponse(history) {
+    setIsLoading(true);
     history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
 
     console.log(history);
@@ -55,6 +67,7 @@ function Chatbox() {
     } catch (error) {
       updateHistory(error.message, true);
     }
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -65,38 +78,69 @@ function Chatbox() {
   }, [chatHistory]);
 
   return (
-    <div className="w-full flex items-center justify-center min-h-[100vh]">
-      <div className="bg-gray-500/40 w-[600px] border-2 rounded-[15px] relative flex flex-col h-[600px]">
-        <div className="bg-gray-600/50 flex items-center justify-between py-4 px-5">
-          <div className="flex gap-3 items-center">
-            <h2 className="text-lg font-semibold text-white">Chatbot</h2>
+    <>
+      {/* <div className="w-full flex items-center justify-center min-h-[100vh]">
+        <div className="bg-gray-500/40 w-[600px] border-2 rounded-[15px] relative flex flex-col h-[600px]">
+          <div className="bg-gray-600/50 flex items-center justify-between py-4 px-5">
+            <div className="flex gap-3 items-center">
+              <h2 className="text-lg font-semibold text-white">Chatbot</h2>
+            </div>
           </div>
-        </div>
 
-        <div
-          ref={ref}
-          className="p-5 overflow-y-auto flex flex-col gap-4 flex-grow"
-        >
-          <div className="flex items-center gap-5">
-            <Bot color="white" />
-            <p className="break-words p-2 whitespace-pre-line border-2 border-gray-500 rounded-lg text-sm text-white">
-              Hey there, I'm Riad! Ask anything about me & my projects.
-            </p>
+          <div
+            ref={ref}
+            className="p-5 overflow-y-auto flex flex-col gap-4 flex-grow"
+          >
+            <div className="flex items-center gap-5">
+              <Bot color="white" />
+              <p className="break-words p-2 whitespace-pre-line border-2 border-gray-500 rounded-lg text-sm text-white">
+                Hey there, I'm Riad! Ask anything about me & my projects.
+              </p>
+            </div>
+            {chatHistory.map((chat, index) => (
+              <ChatMessage key={index} chat={chat} />
+            ))}
           </div>
-          {chatHistory.map((chat, index) => (
-            <ChatMessage key={index} chat={chat} />
-          ))}
-        </div>
 
-        <div className="w-full bg-white p-3">
+          <div className="w-full bg-white p-3"></div>
+        </div>
+      </div> */}
+
+      <section className="p-4">
+        <div className="m-auto flex flex-col gap-4  max-w-3xl bg-gray-400/20 p-4 rounded-md ">
+          <div
+            ref={ref}
+            className="flex flex-col gap-4 overflow-y-auto h-[300px]"
+          >
+            {chatHistory.map((chat, index) => (
+              <div
+                className={`p-4 max-w-[80%] rounded-2xl text-white ${
+                  chat.role === "model"
+                    ? "bg-slate-500 text-left self-start rounded-bl-none"
+                    : " text-right bg-purple-500 self-end rounded-br-none"
+                }`}
+                key={index}
+              >
+                <motion.h1 className="z-50 text-black">{chat.text}</motion.h1>
+              </div>
+            ))}
+            {loading && (
+              <div className="flex w-full items-end justify-center text-white">
+                <Dot className="animate-bounce text-white" />
+                <Dot className="animate-bounce text-white" />
+                <Dot className="animate-bounce text-white" />
+              </div>
+            )}
+          </div>
           <ChatForm
             chatHistory={chatHistory}
             setChatHistory={setChatHistory}
             generateBotResponse={generateBotResponse}
+            loading={loading}
           />
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
 
--- components/ui/chatbot/ChatForm.jsx
+++ components/ui/chatbot/ChatForm.jsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { ArrowUp } from "lucide-react";
+import { ArrowUp, Forward } from "lucide-react";
 import { useRef } from "react";
 
-function ChatForm({ chatHistory, setChatHistory, generateBotResponse }) {
+function ChatForm({
+  chatHistory,
+  setChatHistory,
+  generateBotResponse,
+  loading,
+}) {
   const inputRef = useRef();
 
   function handleFormSubmit(e) {
@@ -22,33 +27,66 @@ function ChatForm({ chatHistory, setChatHistory, generateBotResponse }) {
       { role: "user", text: userMessage },
     ]);
 
-    function updateHistory(text, isError = false) {
-      setChatHistory((prev) => [...prev, { role: "model", text, isError }]);
-    }
-
     // Calls function that generates the response
-    generateBotResponse(
-      [...chatHistory, { role: "user", text: userMessage }],
-      updateHistory
-    );
+    generateBotResponse([...chatHistory, { role: "user", text: userMessage }]);
   }
 
   return (
-    <form
-      action="#"
-      onSubmit={handleFormSubmit}
-      className="flex items-center bg-white outline outline-gray-400 rounded-lg"
-    >
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder="Message..."
-        required
-        className="border-none outline-none flex-grow bg-none h-[47px] p-2 text-sm"
-      />
-      <button className="flex justify-center items-center text-white h-8 w-8 border-none outline-none cursor-pointer shrink-0 mr-2 rounded-full bg-gray-700">
-        <ArrowUp className="w-6 h-5" />
-      </button>
+    // <form
+    //   action="#"
+    //   onSubmit={handleFormSubmit}
+    //   className="flex items-center bg-white outline outline-gray-400 rounded-lg"
+    // >
+    //   <input
+    //     ref={inputRef}
+    //     type="text"
+    //     placeholder="Message..."
+    //     required
+    //     className="border-none outline-none flex-grow bg-none h-[47px] p-2 text-sm"
+    //   />
+    //   <button className="flex justify-center items-center text-white h-8 w-8 border-none outline-none cursor-pointer shrink-0 mr-2 rounded-full bg-gray-700">
+    //     <ArrowUp className="w-6 h-5" />
+    //   </button>
+    // </form>
+
+    // <form className="flex items-center" onSubmit={handleFormSubmit}>
+    //   <input
+    //     ref={inputRef}
+    //     className="flex-1 border rounded rounded-r-none border-gray-400 py-2 px-4"
+    //     required
+    //     placeholder="Message..."
+    //     type="text"
+    //   />
+    //   <button
+    //     disabled={loading}
+    //     className={`px-4 py-2 bg-blue-500 rounded-lg rounded-l-none ${loading} ? bg-blue-300 :bg-blue-500 `}
+    //     type="submit"
+    //   >
+    //     Send
+    //   </button>
+    // </form>
+
+    <form onSubmit={handleFormSubmit} className="w-full">
+      <div className="relative mt-4 flex">
+        <div className="absolute top-0 flex w-full justify-center">
+          <div className="h-[1px] w-full animate-border-width rounded-full  bg-gradient-to-r from-[rgba(17,17,17,0)] via-white to-[rgba(17,17,17,0)] transition-all duration-1000" />
+        </div>
+        <input
+          ref={inputRef}
+          name="question"
+          id="question"
+          className="block h-12 w-[95%] rounded-md rounded-r-none  border border-gray-800 bg-gray-950 px-4 py-2 text-gray-300 focus:outline-none md:px-6"
+        />
+        <button
+          type="submit"
+          className={`flex h-12 w-[15%] items-center justify-center rounded-r-md border border-gray-800 bg-gradient-to-r from-black to-gray-950 md:w-[10%] ${
+            loading ? "cursor-not-allowed" : ""
+          }`}
+          disabled={loading}
+        >
+          <Forward className="text-white" />
+        </button>
+      </div>
     </form>
   );
 }
