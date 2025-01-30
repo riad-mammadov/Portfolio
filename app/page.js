f5f212ebba295aca14335a0781ebb926f83f537c
Riad <riad_mammadov@yahoo.com>
2025-01-30 15:50:41 +0000

    adjust page layout

--- app/page.js
+++ app/page.js
@@ -1,22 +1,26 @@
 import MainSection from "@/components/Section/intro/MainSection";
 import ProjectSection from "@/components/Section/project/ProjectSection.jsx";
-import Timeline from "@/components/Section/Timeline";
+import Timeline from "@/components/Section/timeline/Timeline";
 import Chatbox from "@/components/Section/chatbot/Chat";
+import Footer from "@/components/ui/Footer";
 export default function Home() {
   return (
     <main>
-      <MainSection />
-      <div className="space-y-44">
-        <section id="project-display">
+      <section id="Home">
+        <MainSection />
+      </section>
+      <div className="sm:space-y-44">
+        <section id="Portfolio">
           <ProjectSection />
         </section>
-        <section id="timeline-section">
+        <section id="Experience">
           <Timeline />
         </section>
-        <section>
+        <section className="About">
           <Chatbox />
         </section>
       </div>
+      <Footer />
     </main>
   );
 }
