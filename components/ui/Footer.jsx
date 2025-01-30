f0a6cf0cb4aa0f2775fbb640c3b72ada4dfeebd2
Riad <riad_mammadov@yahoo.com>
2025-01-30 23:02:37 +0000

    add page footer

---
+++ components/ui/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="w-full flex items-center justify-center bg-transparent">
+      <h1 className="font-roboto text-sm mb-4 sm:text-lg text-white">
+        © 2025 Crafted with 💙 by Riad using
+        <span className="text-blue-500"> Next</span>
+      </h1>
+    </footer>
+  );
+}
+
+export default Footer;
