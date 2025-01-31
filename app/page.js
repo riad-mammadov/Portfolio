e8038ec9768e7227eda30f1ff7748d7488653365
Riad <riad_mammadov@yahoo.com>
2025-01-31 20:51:11 +0000

    add contact section

--- app/page.js
+++ app/page.js
@@ -3,21 +3,25 @@ import ProjectSection from "@/components/Section/project/ProjectSection.jsx";
 import Timeline from "@/components/Section/timeline/Timeline";
 import Chatbox from "@/components/Section/chatbot/Chat";
 import Footer from "@/components/ui/Footer";
+import ContactForm from "@/components/Section/Contact/ContactForm";
 export default function Home() {
   return (
     <main>
       <section id="Home">
         <MainSection />
       </section>
-      <div className="sm:space-y-44">
-        <section id="Portfolio">
+      <div className="sm:space-y-20 mb-12">
+        <section id="Projects">
           <ProjectSection />
         </section>
         <section id="Experience">
           <Timeline />
         </section>
-        <section className="About">
+        <section id="About">
           <Chatbox />
+        </section>
+        <section id="Contact">
+          <ContactForm />
         </section>
       </div>
       <Footer />
