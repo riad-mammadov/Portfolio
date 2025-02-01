4f92caa3ef07b46242984c096a952a6a20e0b99a
Riad <riad_mammadov@yahoo.com>
2025-02-01 19:42:16 +0000

    change screen sizes, add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,19 +9,16 @@ export default {
   theme: {
     screens: {
       sm: "450px",
+
       "sm/m": "570px",
 
       md: "768px",
-      // => @media (min-width: 768px) { ... }
 
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
 
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
 
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       backgroundImage: {
