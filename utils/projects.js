05099f6a20693ebfb6539cffeb415aff32a6914b
Riad <riad_mammadov@yahoo.com>
2025-02-24 20:51:56 +0000

    add projects

--- utils/projects.js
+++ utils/projects.js
@@ -1,8 +1,7 @@
 export const projects = [
   {
     title: "Portfolio",
-    description: "My personal portfolio website built with Next.js",
-    image: "/portfolioImg.png",
+    description: "My personal portfolio website built with Next.js.",
     githubUrl: "https://github.com/riad-mammadov/Portfolio",
     techStack: [
       { name: "Next.Js", icon: "/nextjs_icon_dark.svg" },
@@ -17,8 +16,7 @@ export const projects = [
   {
     title: "QAHub",
     description:
-      "A hub for muslims to read the Quran, track their progress and test their arabic skills (still in development)",
-    image: "/javascript.svg",
+      "A hub for muslims to read the Quran, track their progress and test their arabic skills (still in development).",
     githubUrl: "https://github.com/riad-mammadov/QAHub",
     techStack: [
       { name: "Next.Js", icon: "/nextjs_icon_dark.svg" },
@@ -30,63 +28,78 @@ export const projects = [
     ],
   },
   {
+    title: "Serve2U",
+    description:
+      "Web application for football fans to order drinks and snacks directly to their seats, utilising Stripe API for payments. Also contains a staff dashboard to oversee application and manage orders.",
+    githubUrl: "https://github.com/username/project-two",
+    detailsUrl: "/projects/project-two",
+    techStack: [
+      { name: "JavaScript", icon: "/javascript.svg" },
+      { name: "Node.Js", icon: "/nodejs.svg" },
+      { name: "Express.Js", icon: "/expressjs.svg" },
+      { name: "PHP", icon: "/php_dark.svg" },
+      { name: "MySQL", icon: "/mysql.svg" },
+      { name: "Stripe", icon: "/stripe.svg" },
+    ],
+  },
+  {
     title: "Clinic Express",
     description:
-      "Web based cloud application to manage patient data & book appointments",
-    image: "/javascript.svg",
+      "Web based cloud application to manage patient data & book appointments, utilising GCP technologies.",
     githubUrl: "https://github.com/username/project-two",
     techStack: [
-      { name: "React", icon: "/react.svg" },
-      { name: "TypeScript", icon: "/typescript.svg" },
-      { name: "Next.js", icon: "/next.svg" },
+      { name: "HTML", icon: "/html5.svg" },
+      { name: "CSS", icon: "/css.svg" },
+      { name: "Google Cloud Platform", icon: "/google-cloud.svg" },
+      { name: "Node.js", icon: "/nodejs.svg" },
+      { name: "Express.js", icon: "/expressjs.svg" },
     ],
   },
   {
-    title: "Project 4",
-    description: "React Native mobile app with custom animations",
-    image: "/javascript.svg",
-    githubUrl: "https://github.com/username/project-two",
-    detailsUrl: "/projects/project-two",
+    title: "AirVia TMS",
+    description:
+      "A ticket sales management system to streamline & automate the process of managing ticket sales",
+    githubUrl: "https://github.com/riad-mammadov/AirVia-TicketSalesSystem",
     techStack: [
-      { name: "React", icon: "/react.svg" },
-      { name: "TypeScript", icon: "/typescript.svg" },
-      { name: "Next.js", icon: "/next.svg" },
+      { name: "HTML", icon: "/html5.svg" },
+      { name: "Java", icon: "/java.svg" },
+      { name: "MySQL", icon: "/mysql.svg" },
     ],
   },
   {
-    title: "Project 5",
-    description: "React Native mobile app with custom animations",
-    image: "/javascript.svg",
-    githubUrl: "https://github.com/username/project-two",
+    title: "React: The Complete Guide",
+    description:
+      "A comprehensive 71 hour React course on Udemy, going into detail on all things React & NextJs",
+    githubUrl:
+      "https://github.com/riad-mammadov/React-The-Complete-Guide-Course",
+    certification:
+      "https://www.udemy.com/certificate/UC-3db88c29-e8d2-49a2-9534-b23db38d27d6/",
     detailsUrl: "/projects/project-two",
     techStack: [
       { name: "React", icon: "/react.svg" },
+      { name: "Next.Js", icon: "/nextjs_icon_dark.svg" },
+      { name: "PostgreSQL", icon: "/postgresql.svg" },
+      { name: "Tailwind CSS", icon: "/tailwindcss.svg" },
       { name: "TypeScript", icon: "/typescript.svg" },
-      { name: "Next.js", icon: "/next.svg" },
+      { name: "Redux", icon: "/redux.svg" },
+      { name: "Framer Motion", icon: "/framer.png" },
     ],
   },
   {
-    title: "Project 6",
-    description: "React Native mobile app with custom animations",
-    image: "/javascript.svg",
-    githubUrl: "https://github.com/username/project-two",
-    detailsUrl: "/projects/project-two",
+    title: "Ultimate Web Development Course",
+    description: "A 61 hour full stack Web Development course on Udemy",
+    githubUrl:
+      "https://github.com/riad-mammadov/The-Complete-Web-Development-Course",
+    certification:
+      "https://www.udemy.com/certificate/UC-6a74bc3e-549c-4be8-90f0-4a9d2b479714/",
     techStack: [
-      { name: "React", icon: "/react.svg" },
-      { name: "TypeScript", icon: "/typescript.svg" },
-      { name: "Next.js", icon: "/next.svg" },
-    ],
-  },
-  {
-    title: "Project 7",
-    description: "React Native mobile app with custom animations",
-    image: "/javascript.svg",
-    githubUrl: "https://github.com/username/project-two",
-    detailsUrl: "/projects/project-two",
-    techStack: [
-      { name: "React", icon: "/react.svg" },
-      { name: "TypeScript", icon: "/typescript.svg" },
-      { name: "Next.js", icon: "/next.svg" },
+      { name: "JavaScript", icon: "/javascript.svg" },
+      { name: "EJS", icon: "/ejs.png" },
+      { name: "Node.js", icon: "/nodejs.svg" },
+      { name: "Express.js", icon: "/expressjs.svg" },
+      { name: "Postman", icon: "/postman.svg" },
+      { name: "Axios", icon: "/axios.png" },
+      { name: "OAuth", icon: "/oauth.png" },
     ],
   },
 ];
