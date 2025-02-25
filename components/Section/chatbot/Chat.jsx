a24ff12e224443d7ccfc54613d22b95131702c88
Riad <riad_mammadov@yahoo.com>
2025-02-25 14:42:19 +0000

    deploy

--- components/Section/chatbot/Chat.jsx
+++ components/Section/chatbot/Chat.jsx
@@ -45,6 +45,7 @@ function Chat() {
       });
       const data = await response.json();
       if (!response.ok) {
+        console.log(data.error);
         throw new Error(data.error.message || "Something went wrong!");
       }
       const apiResponseText = data.candidates[0].content.parts[0].text;
