946a2294316ffa1aee985f8a84eb7b6390a18401
Riad <riad_mammadov@yahoo.com>
2025-07-01 19:11:45 +0100

    adjust font size

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center bg-transparent">
-      <h1 className="font-roboto text-sm mb-4 sm:text-lg text-white">
+    <footer className="w-full flex items-center justify-center bg-transparent mt-10">
+      <h1 className="font-roboto text-sm mb-4 sm:text-base text-white">
         © 2025 Crafted with 💙 by Riad using
-        <span className="text-blue-500"> Next</span>
+        <span className="text-blue-500"> Next.js</span>
       </h1>
     </footer>
   );
