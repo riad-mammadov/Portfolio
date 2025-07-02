5946d24e1a55b93b214dce2c58b9c2c5c737fec5
Riad <riad_mammadov@yahoo.com>
2025-07-02 19:47:02 +0100

    fix blur

--- components/Section/chatbot/Chat.jsx
+++ components/Section/chatbot/Chat.jsx
@@ -74,7 +74,7 @@ function Chat() {
       <div className="flex flex-col items-center justify-center h-[400px]">
         <article
           ref={ref}
-          className="w-[90%] md:w-[75%] h-[100%] overflow-y-auto rounded-lg border border-gray-800 bg-gradient-to-bl backdrop-blur-md from-slate-950/80 to-gray-950/80 shadow-md p-6"
+          className="w-[90%] md:w-[75%] h-[100%] overflow-y-auto rounded-lg border border-gray-800 bg-gradient-to-bl from-slate-950/80 to-gray-950/80 shadow-md p-6"
         >
           <div
             className="flex flex-col gap-2"
@@ -101,7 +101,7 @@ function Chat() {
           )}
         </article>
       </div>
-      <div className="w-full px-[5%] sm:px-[20%] backdrop-blur-lg">
+      <div className="w-full px-[5%] sm:px-[20%]">
         <ChatForm
           chatHistory={chatHistory}
           setChatHistory={setChatHistory}
